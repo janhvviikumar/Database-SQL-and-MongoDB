1a8264441b5aa2c377cc3bfa3ea348c63e62b7c3
janhvviikumar <janhvviik@gmail.com>
2025-12-04 22:53:26 +0530

    Time: 326 ms (87.3%), Space: 67.7 MB (93.21%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.py
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
-    df=daily_sales
-    n=df.groupby(["date_id","make_name"]).nunique().reset_index()
-    n.columns = ['date_id', 'make_name', 'unique_leads', 'unique_partners']
-    return n
+    grouped = daily_sales.groupby(['date_id', 'make_name']).agg({'lead_id': 'nunique', 'partner_id': 'nunique'}).reset_index()
+    grouped.columns = ['date_id', 'make_name', 'unique_leads', 'unique_partners']
+    
+    return grouped
 
     
